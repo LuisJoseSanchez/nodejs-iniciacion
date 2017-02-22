61a6c8e6a69529e9603c61f07ce49e962bd5974b
Luis José Sánchez <luisjoseprofe@gmail.com>
2017-02-22 22:44:55 +0100

    Actualizado el Hola Mundo

--- 01HolaMundo/server.js
+++ 01HolaMundo/server.js
@@ -2,11 +2,11 @@ var http = require("http");
 
 function onRequest(request, response) {
   console.log("Peticion Recibida.");
-  response.writeHead(200, {"Content-Type": "text/html"});
+  response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
   response.write("¡Hola mundo!");
   response.end();
 }
 
 http.createServer(onRequest).listen(8888);
 
-console.log("Servidor Iniciado.");
+console.log("Servidor iniciado en http://localhost:8888");
