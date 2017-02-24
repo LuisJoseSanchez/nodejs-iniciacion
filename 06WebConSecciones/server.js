c4c13b687bc4c31d9563292bbf07b6ebaafed858
Luis José Sánchez <luisjoseprofe@gmail.com>
2017-02-24 09:23:10 +0100

    Actualizando ejemplos

--- 06WebConSecciones/server.js
+++ 06WebConSecciones/server.js
@@ -4,7 +4,7 @@ var url = require("url");
 function iniciar(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    response.writeHead(200, {"Content-Type": "text/html"});
+    response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
     response.write(route(pathname, handle));
     response.end();
   }
